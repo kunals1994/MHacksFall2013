08337bfbd94e7b8af316f7e1d0a5891cda3dd585
Kevin Young <kyoung18@terpmail.umd.edu>
2013-09-21 00:51:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotionCapture::Application.config.secret_key_base = 'aa50651cd674e47fe6b20c62f9e6c9344ffe6b78a8687126761b3d2d9d00db92d5de6e6c6d4fbf4c3a9fd3a1ef01dd37f85055e6b431a48e596eb4d21c373f31'
